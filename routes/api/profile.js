d92535397233d1a6e22abfbbce97fcb165dbb17f
Donna <dlambertpdx@gmail.com>
2018-07-19 17:10:19 -0700

    add post, comments, likes routes

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -278,26 +278,14 @@ router.delete(
 // @desc    Delete user and profile
 // @access  Private
 router.delete(
-  "/education/:edu_id",
+  "/",
   passport.authenticate("jwt", { session: false }),
   (req, res) => {
-    const { errors, isValid } = validateEducationInput(req.body);
-
-    profile
-      .findOne({ user: req.user.id })
-      .then(profile => {
-        // Get remove index
-        const removeIndex = profile.education
-          .map(item => item.id)
-          .indexOf(req.params.edu_id);
-
-        // Splice out of array
-        profile.education.splice(removeIndex, 1);
-
-        // Save
-        profile.save().then(profile => res.json(profile));
-      })
-      .catch(err => res.status(404).json(err));
+    profile.findOneAndRemove({ user: req.user.id }).then(() => {
+      User.findOneAndRemove({ _id: req.user.id }).then(() =>
+        res.json({ success: true })
+      );
+    });
   }
 );
 
